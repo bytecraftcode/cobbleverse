911152d64a0a61a247b58589be1807bf61d64abd
bytecraftcode <bytecraftcode003@gmail.com>
2026-01-18 23:03:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('btnMega').addEventListener('click', function(e) {
+    console.log("Redirigiendo a Mega para descargar Cobbleverse...");
+    // Aquí podrías añadir una alerta si quisieras:
+    // alert("Estás siendo redirigido a la descarga.");
+});
